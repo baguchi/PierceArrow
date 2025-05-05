f127557f4f4698f7594eb95fc2224c8085bbcbe8
iron431 <34083081+iron431@users.noreply.github.com>
2025-05-05 13:41:01 -0500

    port to 1.21.1

--- src/main/java/baguchan/piercearrow/PierceArrow.java
+++ src/main/java/baguchan/piercearrow/PierceArrow.java
@@ -1,16 +1,17 @@
 package baguchan.piercearrow;
 
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 
-// The value here should match an entry in the META-INF/mods.toml file
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(PierceArrow.MODID)
 public class PierceArrow
 {
     public static final String MODID = "piercearrow";
 
-    public PierceArrow(IEventBus modBusEvent) {
+    public PierceArrow(IEventBus modBusEvent, ModContainer modContainer) {
         // Register the setup method for modloading
         modBusEvent.addListener(this::setup);
 
